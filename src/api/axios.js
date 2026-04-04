08c3631e4684b857d0b479b20f912b64b4b3f9f8
Paridhisharmagithub <sparidhi161.ps@gmail.com>
2026-04-05 02:33:38 +0530

    updating api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://finance-dashboard-backend-gf2z.onrender.com"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 API.interceptors.request.use((req) => {
